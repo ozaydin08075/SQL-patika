960ce292d5a7b17f8dcff07e44d74f062d700549
Kamil Eray Özaydın <65467021+ozaydin08075@users.noreply.github.com>
2026-02-10 13:38:43 +0300

    Add files via upload

---
+++ Ödev-10/SQL.sql
@@ -0,0 +1,11 @@
+--1
+SELECT city , country FROM country
+LEFT JOIN city ON country.country_id = city.country_id ;
+
+--2
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
+
+--3
+SELECT rental.rental_id , customer.first_name, customer.last_name FROM customer
+FULL JOIN rental ON rental.customer_id = customer.customer_id ;
